62f0f8ee3612cbbd81b1b8a9a3cdf9c122a0da76
teohweijing <ryanteoh0426@gmail.com>
2016-03-02 14:47:36 +0800

    Changed Seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,16 @@
 r1 = Role.create({name: "Regular", description: "Can read items"})
 r2 = Role.create({name: "Admin", description: "Can perform any CRUD operation on any resource"})
+r3 = Role.create({name: "Super Admin", description: "Can perform any CRUD activities, including creating admin account"})
 
-g1 = Application.create({name: "Poker", image: "#"})
-g2 = Application.create({name: "Blackjack", image: "#"})
-g3 = Application.create({name: "Roulette", image: "#"})
+# g1 = Application.create({name: "Poker", image: "#"})
+# g2 = Application.create({name: "Blackjack", image: "#"})
+# g3 = Application.create({name: "Roulette", image: "#"})
 
 u1 = User.create({name: "Ryan", email: "ryan@example.com", password:"aaaaaaaa", password_confirmation: "aaaaaaaa", role_id: r2.id})
 u2 = User.create({name: "Sue", email: "sue@example.com", password: "aaaaaaaa", password_confirmation: "aaaaaaaa", role_id: r1.id})
 u3 = User.create({name: "Kev", email: "kev@example.com", password: "aaaaaaaa", password_confirmation: "aaaaaaaa", role_id: r1.id})
 u4 = User.create({name: "Jack", email: "jack@example.com", password: "aaaaaaaa", password_confirmation: "aaaaaaaa", role_id: r1.id})
 
-a1 = Account.create({username: "sue", password: "password", user_id: u2.id, application_id: g1.id})
-a2 = Account.create({username: "kev", password: "password", user_id: u3.id, application_id: g2.id})
-a3 = Account.create({username: "jack", password: "password", user_id: u4.id, application_id: g3.id})
+# a1 = Account.create({username: "sue", password: "password", user_id: u2.id, application_id: g1.id})
+# a2 = Account.create({username: "kev", password: "password", user_id: u3.id, application_id: g2.id})
+# a3 = Account.create({username: "jack", password: "password", user_id: u4.id, application_id: g3.id})
