cf6a6e90f18ca3045732cc514dc15b07872d7fe9
teohweijing <ryanteoh0426@gmail.com>
2016-03-02 16:42:22 +0800

    MEssage

--- config/application.rb
+++ config/application.rb
@@ -25,6 +25,5 @@ module Game
 
     # Needed for Devise gem
     config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-    config.assets.initialize_on_precompile = true
   end
 end
